f25838910edac6626eacc401efbd545d8ab0aefb
Tokio-svg <tokkipt935@gmail.com>
2022-02-14 12:02:17 +0900

    移動可能位置ハイライト追加

--- src/js/game.js
+++ src/js/game.js
@@ -9,9 +9,29 @@ const game = new Chess();
 const calcGame = new Chess();
 
 // ---------------------------------------------
+// 設定
+// ---------------------------------------------
+const cfg = {
+  draggable: true,
+  position: 'start',
+  // 駒が移動した後にonDrop関数を実行する
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd,
+  onMouseoutSquare: removeHighlight,
+  onMouseoverSquare: onMouseoverSquare,
+};
+
+// ---------------------------------------------
+// ChessBoadインスタンス作成
+// ---------------------------------------------
+const board = new ChessBoard('board', cfg);
+
+// ---------------------------------------------
 // 駒が置かれた場合の処理
 // ---------------------------------------------
-const onDrop = function (source, target) {
+function onDrop(source, target) {
+  removeHighlight();
+
   // 駒の移動パターンをチェックする
   const move = game.move({
     from: source,  // 移動元の位置
@@ -33,19 +53,11 @@ const onDrop = function (source, target) {
 };
 
 // ---------------------------------------------
-// 設定
+// 盤面更新処理
 // ---------------------------------------------
-const cfg = {
-  draggable: true,
-  position: 'start',
-  // 駒が移動した後にonDrop関数を実行する
-  onDrop: onDrop
-};
-
-// ---------------------------------------------
-// ChessBoadインスタンス作成
-// ---------------------------------------------
-const board = new ChessBoard('board', cfg);
+function onSnapEnd () {
+  board.position(game.fen());
+}
 
 // ---------------------------------------------
 // ボタン操作処理
@@ -61,19 +73,50 @@ $('#resetBtn').on('click', () => {
 $('#flipBtn').on('click', board.flip);
 
 // ---------------------------------------------
+// 移動可能位置ハイライト処理
+// ---------------------------------------------
+// 変更後の色指定
+const whiteSquareGrey = '#a9a9a9';
+const blackSquareGrey = '#696969';
+
+// マウスオーバー時
+function onMouseoverSquare (square, piece) {
+  const moves = game.moves({
+    square: square,
+    verbose: true,
+  })
+  if (moves.length === 0) return;
+  highlightSquare(square);
+  for (let i = 0; i < moves.length; i++) {
+    highlightSquare(moves[i].to);
+  }
+}
+
+// 色変更処理
+function highlightSquare (square) {
+  const $square = $('#board .square-' + square);
+
+  let background = whiteSquareGrey;
+  if ($square.hasClass('black-3c85d')) background = blackSquareGrey;
+
+  $square.css('background', background);
+}
+
+// ハイライト解除処理
+function removeHighlight () {
+  $('#board .square-55d63').css('background', '')
+}
+
+// ---------------------------------------------
 // CPUプレイヤーの移動処理
 // ---------------------------------------------
 async function makeCPUmove() {
-
   const cpuMove = await calcMove();
 
   if (!cpuMove) return;
 
   // 選択した場所へ駒を動かす
   game.move(cpuMove, { promotion: 'q' });
-
-  board.position(game.fen());
-
 }
 
 // ---------------------------------------------
@@ -118,6 +161,30 @@ async function calcMove() {
 // ---------------------------------------------
 // 盤面評価処理
 // ---------------------------------------------
+
+// ピース基本点
+const PIECE_SCORE = {
+  'p': 1000,
+  'n': 3000,
+  'b': 3500,
+  'r': 5000,
+  'q': 9000,
+  'k': 1000000000,
+}
+
+// 位置による補正(indexはchess.board()と対応)
+const POS_SCORE = [
+  1, 1, 1,   1,   1,   1,   1, 1,
+  1, 1, 1,   1,   1,   1,   1, 1,
+  1, 1, 1.5, 1.5, 1.5, 1.5, 1, 1,
+  1, 1, 1.5, 2,   2,   1.5, 1, 1,
+  1, 1, 1.5, 2,   2,   1.5, 1, 1,
+  1, 1, 1.5, 1.5, 1.5, 1.5, 1, 1,
+  1, 1, 1,   1,   1,   1,   1, 1,
+  1, 1, 1,   1,   1,   1,   1, 1
+]
+
+// 評価関数
 function evaluate(board, color) {
   let score = 0;
   for (const raw of board) {
@@ -129,13 +196,4 @@ function evaluate(board, color) {
     }
   }
   return score;
-}
-
-const PIECE_SCORE = {
-  'p': 1000,
-  'n': 3000,
-  'b': 3500,
-  'r': 5000,
-  'q': 9000,
-  'k': 1000000000,
 }
